a670d9e89a5f2ebfecc1558baebf928c01f80d62
hwangyoonjae <hyj90632@gmail.com>
2021-10-30 00:32:34 +0900

    navbar color  변경

--- src/components/Webpage/style.vue
+++ src/components/Webpage/style.vue
@@ -5,39 +5,44 @@
         <el-radio-button :label="false">expand</el-radio-button>
         <el-radio-button :label="true">collapse</el-radio-button>
       </el-radio-group>
-      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span slot="title">Navigator One</span>
+            <span>Navigator One</span>
           </template>
-          <el-menu-item-group>
+          <el-menu-item-group title="Group One">
             <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
+            <el-menu-item index="1-2">item one</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="Group Two">
             <el-menu-item index="1-3">item three</el-menu-item>
           </el-menu-item-group>
           <el-submenu index="1-4">
-            <span slot="title">item four</span>
+            <template slot="title">item four</template>
             <el-menu-item index="1-4-1">item one</el-menu-item>
           </el-submenu>
         </el-submenu>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
-          <span slot="title">Navigator Two</span>
+          <span>Navigator Two</span>
         </el-menu-item>
         <el-menu-item index="3" disabled>
           <i class="el-icon-document"></i>
-          <span slot="title">Navigator Three</span>
+          <span>Navigator Three</span>
         </el-menu-item>
         <el-menu-item index="4">
           <i class="el-icon-setting"></i>
-          <span slot="title">Navigator Four</span>
+          <span>Navigator Four</span>
         </el-menu-item>
       </el-menu>
     </div>
     <div class="style_component_table_group">
+      <Board></Board>
+      <Todo></Todo>
+    </div>
+    <div class="style_component_todo_group">
+      <Todo></Todo>
       <Board></Board>
     </div>
   </div>
@@ -76,12 +81,25 @@ export default {
   margin-top: 20px;
 }
 
+.style_component_menu_group {
+  height: 100%;
+}
+
 .style_component_menu_radio {
   margin-bottom: 20px;
   display: flex;
 }
 
+.el-menu-vertical-demo {
+  background: #545c64;
+}
+
 .style_component_table_group {
+  width: 100%;
+  margin-left: 10px;
+}
+
+.style_component_todo_group {
   width: 100%;
   margin-left: 10px;
 }
