7b71f30c9478375147e3492f6dc2a1a43953215c
hwangyoonjae <hyj90632@gmail.com>
2021-11-03 23:11:05 +0900

    git clone

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -5,7 +5,12 @@
         <i class="fas fa-user-friends" aria-hidden="true"></i>
       </span>
       <div class="Card_people_count">
-
+          <el-row :gutter="20">
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
       </div>
     </div>
   </div>
@@ -25,4 +30,27 @@ export default {
   background: linear-gradient(to right, #6478FB, #8763FB);
   width: 3rem;
 }
+.el-row {
+    margin-bottom: 20px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
