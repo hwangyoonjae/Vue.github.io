6a57951817d19b8283bd00d6518b2d2df143d200
hwangyoonjae <hyj90632@gmail.com>
2022-01-08 21:26:47 +0900

    Slide 틀 구성

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="Main_Component">
-    
+    <el-carousel :interval="2000" type="card" height="200px">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3 class="medium">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
   </div>
 </template>
 
@@ -11,5 +15,19 @@
 </script>
 
 <style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
 
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
 </style>
