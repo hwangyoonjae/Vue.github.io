2215ead3b70161546ee7b5e4c24c2a02d8fd4e02
hwangyoonjae <hyj90632@gmail.com>
2022-01-13 01:08:43 +0900

    Sidebar 업무일정 추가

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -10,24 +10,28 @@
     </el-radio-group>
     <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
     <el-menu-item index="1">
-      <i class="el-icon-s-home"></i>
+      <i class="el-icon-house"></i>
       <span slot="title">대시보드</span>
     </el-menu-item>
     <el-menu-item index="2">
-      <i class="el-icon-s-cooperation"></i>
+      <i class="el-icon-magic-stick"></i>
       <span slot="title">프로젝트</span>
     </el-menu-item>
     <el-menu-item index="3">
-      <i class="el-icon-message-solid"></i>
+      <i class="el-icon-bell"></i>
       <span slot="title">공지사항</span>
     </el-menu-item>
     <el-menu-item index="4">
-      <i class="el-icon-s-order"></i>
+      <i class="el-icon-document"></i>
       <span slot="title">게시판</span>
     </el-menu-item>
-    <el-submenu index="5">
+    <el-menu-item index="5">
+      <i class="el-icon-date"></i>
+      <span slot="title">업무일정</span>
+    </el-menu-item>
+    <el-submenu index="6">
       <template slot="title">
-        <i class="el-icon-s-custom"></i>
+        <i class="el-icon-user"></i>
       <span slot="title">내 페이지</span>
       </template>
       <el-menu-item-group>
