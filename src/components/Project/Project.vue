0ca5e519c873a0ff2b4999dfdbce1c8168a0dfbf
hwangyoonjae <hyj90632@gmail.com>
2022-01-17 23:34:29 +0900

    프로젝트 내용 추가

---
+++ src/components/Project/Project.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="Project_component">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="프로젝트명" prop="title"></el-table-column>
+      <el-table-column label="시작일" prop="start"></el-table-column>
+      <el-table-column label="완료일" prop="finish"></el-table-column>
+      <el-table-column label="이슈" prop="problem"></el-table-column>
+      <el-table-column label="상태" prop="state"></el-table-column>
+      <el-table-column label="완료율" prop="success"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      tableData: [
+      {
+        title : 'Blue X-ray Enterprise',
+        start : '2016-05-03',
+        finish : '2016-05-04',
+        problem : '2',
+        state : 'soso',
+        success : '88%'
+      },
+      {
+        title : 'Blue X-ray DLP',
+        start : '2016-05-03',
+        finish : '2016-05-04',
+        problem : '0',
+        state : 'good',
+        success : '100%'
+      },
+      {
+        title : '전군PC보안체계',
+        start : '2016-05-03',
+        finish : '2016-05-04',
+        problem : '10',
+        state : 'just',
+        success : '50%'
+      }, 
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Project_component {
+  width: 100%;
+}
+</style>
