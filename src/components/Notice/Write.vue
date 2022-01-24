d262913a21dc9439e7e1eaac71dfb67976a00ae9
hwangyoonjae <hyj90632@gmail.com>
2022-01-24 22:27:17 +0900

    Notice 글쓰기 저장

--- src/components/Notice/Write.vue
+++ src/components/Notice/Write.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="Noticewrite_component">
     <div class="input-group" style="margin-bottom:10px;">
-      <el-input placeholder="제목을 입력하세요" v-model="title" @keyup.enter ="createNotice(title)"></el-input>
-      <el-input placeholder="내용을 입력하세요" v-model="component" @keyup.enter ="createNotice(component)"></el-input>
+      <el-input placeholder="제목을 입력하세요" v-model="title"></el-input>
+      <el-input placeholder="내용을 입력하세요" v-model="component"></el-input>
     </div>
     <div class="button-group">
-      <el-button type="success" @click="createNotice(str)">등록하기</el-button>
+      <el-button type="success" @click="updateMode ? updateContent() : uploadContent()">등록하기</el-button>
       <el-button type="danger" @click="cancle">취소하기</el-button>
     </div>
   </div>
@@ -17,35 +17,44 @@ import data from '@/data'
 export default {
   data() {
     return {
-      str: {
-        title: null,
-        component: null,
-      },
-      title: null,
-      component: null,
-      noticeNumber: 0,
+      title: '',
+      component: '',
+      date: '2021-01-24',
+      name: 'James',
+      updatedAt: null,
+      updateObject: null,
+      updateMode: this.$route.params.number > 0 ? true : false,
+    }
+  },
+  created() {
+    if (this.$route.params.number > 0) {
+      const number = Number(this.$route.params.number)
+      this.updateObject = data.Content.filter(item => item.number === number)[0]
+      this.title = this.updateObject.title;
+      this.component = this.updateObject.component;
     }
   },
   methods:{
-    createNotice:function(val){
-      data.tableData.push({ title: val.title, component: val.component});
-      this.noticeNumber++;
- 
-      this.pageUsers = [[]];
-      this.maxpageNumber = this.noticeNumber / 10 + 1;
-      for(var i = 0; i < this.maxpageNumber; i++)
-        this.pageUsers.push([]);
- 
-        var page = 1;
-        var num = 0;
-        for(var i = 0; i < this.noticeNumber; i++){
-          this.pageUsers[page].push(this.users[i]);
-          num++;
-          if(num == 10){
-            num = 0;
-            page++;
-          }
-        }
+    uploadContent() {
+      let items = data.NoticeContent.sort((a,b) => {return b.number - a.number})
+      const number = items[0].number + 1
+      data.NoticeContent.push({
+        number: number,
+        title: this.title,
+        name: this.name,
+        date: this.date,
+        updated_at: null
+      })
+      this.$router.push({
+        path: '/notice'
+      })
+    },
+    updateContent() {
+      this.updateObject.title = this.title;
+      this.updateObject.component = this.component;
+      this.$router.push({
+        path: '/notice'
+      })
     },
     cancle() {
       this.$router.push({
