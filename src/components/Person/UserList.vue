a6e429094cad6383ecff411d361c8c4f5ca88881
hwangyoonjae <hyj90632@gmail.com>
2022-01-26 22:31:00 +0900

    UserList 데이터 값 저장

---
+++ src/components/Person/UserList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="UserList_component">
+    <!--<el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))" style="width: 100%">-->
+    <el-table :data="items">
+      <el-table-column label="번호" prop="user_id"></el-table-column>
+      <el-table-column label="닉네임" prop="nickname"></el-table-column>
+      <el-table-column label="이름" prop="name"></el-table-column>
+      <el-table-column label="이메일" prop="email"></el-table-column>
+      <el-table-column label="등록일" prop="created_at"></el-table-column>
+    </el-table>
+    <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+  </div>
+</template>
+
+<script>
+import data from '@/data'
+
+export default {
+  data() {
+    let items = data.User.sort((a,b) => {return b.user_id - a.user_id})
+    
+    const user = Number(this.$route.params.user_id);
+    const contentData = data.User.filter(item => item.user_id === user)[0];
+    return {
+        items: items
+      }
+    },
+    methods: 
+    {
+      handleEdit(index, row) {
+        console.log(index, row);
+    },
+      handleDelete(index, row) {
+        console.log(index, row);
+    },
+  },
+}
+</script>
+
+<style>
+.UserList_component {
+  text-align: center;
+}
+</style>
