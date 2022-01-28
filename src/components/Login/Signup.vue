c997e0cf06d27837f336549bb0abfef565e7b3cd
hwangyoonjae <hyj90632@gmail.com>
2022-01-28 23:06:40 +0900

    signup 데이터 userlist에 저장 후 출력

--- src/components/Login/Signup.vue
+++ src/components/Login/Signup.vue
@@ -19,7 +19,7 @@
         <el-input v-model="ruleForm.email"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">회원가입</el-button>
+        <el-button type="primary" @click="updateMode ? updateContent() : uploadContent()">회원가입</el-button>
         <el-button @click="resetForm('ruleForm')">초기화</el-button>
         <el-button type="danger" @click="cancle">취소하기</el-button>
       </el-form-item>
@@ -28,89 +28,90 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      var validateId = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('Please input the ID'));
+import data from '@/data'
+
+export default {
+  data() {
+    var validateId = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input the ID'));
+      } else {
+        if (this.ruleForm.checkId !== '') {
+          this.$refs.ruleForm.validateField('checkId');
+        }
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input the password'));
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input the password again'));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('Two inputs don\'t match!'));
+      } else {
+        callback();
+      }
+    };
+    var checkname = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('Please input the name'));
+      }
+    };
+    return {
+      ruleForm: {
+        id: '',
+        pass: '',
+        checkPass: '',
+        name: '',
+        email: ''
+      },
+      rules: {
+        id: [
+          { validator: validateId, trigger: 'blur' }
+        ],
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: 'blur' }
+        ],
+        name: [
+          { validator: checkname, trigger: 'blur' }
+        ],
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('Success sign up!');
         } else {
-          if (this.ruleForm.checkId !== '') {
-            this.$refs.ruleForm.validateField('checkId');
-          }
-          callback();
+          alert('error submit!!');
+          return false;
         }
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('Please input the password'));
-        } else {
-          if (this.ruleForm.checkPass !== '') {
-            this.$refs.ruleForm.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('Please input the password again'));
-        } else if (value !== this.ruleForm.pass) {
-          callback(new Error('Two inputs don\'t match!'));
-        } else {
-          callback();
-        }
-      };
-      var checkname = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('Please input the name'));
-        }
-      };
-      return {
-        ruleForm: {
-          id: '',
-          pass: '',
-          checkPass: '',
-          name: '',
-          email: ''
-        },
-        rules: {
-          id: [
-            { validator: validateId, trigger: 'blur' }
-          ],
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          name: [
-            { validator: checkname, trigger: 'blur' }
-          ],
-        }
-      };
+      });
     },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('Success sign up!');
-          } else {
-            alert('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
-      uploadContent() {
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    uploadContent() {
       let items = data.User.sort((a,b) => {return b.user_id - a.user_id})
       const user = items[0].user_id + 1
       data.User.push({
         user_id: user,
-        id: this.id,
-        name: this.name,
-        name: this.name,
-        email: this.email,
+        id: this.ruleForm.id,
+        name: this.ruleForm.name,
+        email: this.ruleForm.email,
         created_at: '2018-09-11 11:42:11'
       })
       this.$router.push({
@@ -118,8 +119,9 @@
       })
     },
     updateContent() {
-      this.updateObject.title = this.title;
-      this.updateObject.component = this.component;
+      this.updateObject.id = this.id;
+      this.updateObject.name = this.name;
+      this.updateObject.email = this.email;
       this.$router.push({
         path: '/userlist'
       })
