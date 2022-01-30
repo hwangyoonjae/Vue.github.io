26b26d205be29b08c635b2f5e2568db0827b7873
hwangyoonjae <hyj90632@gmail.com>
2022-01-30 17:35:17 +0900

    AdminList 정렬, 저장 값 수정

--- src/components/Person/AdminList.vue
+++ src/components/Person/AdminList.vue
@@ -2,8 +2,8 @@
   <div class="AdminList_component">
     <!--<el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))" style="width: 100%">-->
     <el-table :data="items">
-      <el-table-column label="번호" prop="admin_number"></el-table-column>
-      <el-table-column label="아이디" prop="admin_id"></el-table-column>
+      <el-table-column label="번호" prop="admin_id"></el-table-column>
+      <el-table-column label="아이디" prop="id"></el-table-column>
       <el-table-column label="이름" prop="name"></el-table-column>
       <el-table-column label="이메일" prop="email"></el-table-column>
       <el-table-column label="등록일" prop="created_at"></el-table-column>
@@ -18,10 +18,10 @@ import data from '@/data'
 
 export default {
   data() {
-    let items = data.Admin.sort((a,b) => {return b.admin_number - a.admin_number})
+    let items = data.Admin.sort((a,b) => {return b.admin_id - a.admin_id})
     
-    const admin = Number(this.$route.params.admin_number);
-    const contentData = data.User.filter(item => item.admin_number === admin)[0];
+    const admin = Number(this.$route.params.admin_id);
+    const contentData = data.User.filter(item => item.admin_id === admin)[0];
     return {
         items: items
       }
