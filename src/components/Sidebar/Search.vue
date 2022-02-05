cbf9124896cf6464a801db6d373fd04415087351
hwangyoonjae <hyj90632@gmail.com>
2022-02-05 14:11:42 +0900

    검색창 애니메이션 적용 위한 초기 구성

---
+++ src/components/Sidebar/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search-wrapper">
+    <div class="input-holder">
+        <el-input placeholder="Type to Search" v-model="input" clearable></el-input>
+        <el-row class="search-icon" onclick="searchToggle(this, event);">
+            <el-button icon="el-icon-search" circle></el-button>
+        </el-row>
+    </div>
+    <span class="close" onclick="searchToggle(this, event);"></span>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            input: '',
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
