bcee60ed4b18b0e9371bf37c9c2eb98dfad62694
hwangyoonjae <hyj90632@gmail.com>
2022-02-05 13:42:28 +0900

    dev 서버 실행 시 main페이지 뜨도록 수정

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Board from '@/components/Board/Board'
 import ContentDetail from '@/components/Board/ContentDetail'
 import Create from '@/components/Board/Create'
-import Main from '@/components/Main/Main'
+
 import Notice from '@/components/Notice/Notice'
 import NoticeWrite from '@/components/Notice/Write'
 import Project from '@/components/Project/Project'
@@ -19,77 +19,80 @@ import test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/board/free',
-      name: 'Board',
-      component: Board
-    },
-    {
-      path: '/board/free/detail/:contentId',
-      name: 'ContentDetail',
-      component: ContentDetail
-    },
-    {
-      path: '/board/free/create/:contentId?', // ?는 선택적으로
-      name: 'Create',
-      component: Create
-    },
-    {
-      path: '/main',
-      name: 'Main',
-      component: Main
-    },
-    {
-      path: '/main/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/main/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/todo',
-      name: 'Todo',
-      component: Todo
-    },
-    {
-      path: '/timetable',
-      name: 'Schedule',
-      component: Schedule
-    },
-    {
-      path: '/notice',
-      name: 'Notice',
-      component: Notice
-    },
-    {
-      path: '/notice/write',
-      name: 'NoticeWrite',
-      component: NoticeWrite
-    },
-    {
-      path: '/project',
-      name: 'Project',
-      component: Project
-    },
-    {
-      path: '/userlist',
-      name: 'UserList',
-      component: UserList
-    },
-    {
-      path: '/adminlist',
-      name: 'AdminList',
-      component: AdminList
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/main',
+        },
+        {
+            path: '/main',
+            component: () =>
+                import ('@/components/Main/Main'),
+        },
+        {
+            path: '/board/free',
+            name: 'Board',
+            component: Board
+        },
+        {
+            path: '/board/free/detail/:contentId',
+            name: 'ContentDetail',
+            component: ContentDetail
+        },
+        {
+            path: '/board/free/create/:contentId?', // ?는 선택적으로
+            name: 'Create',
+            component: Create
+        },
+        {
+            path: '/main/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/main/signup',
+            name: 'Signup',
+            component: Signup
+        },
+        {
+            path: '/todo',
+            name: 'Todo',
+            component: Todo
+        },
+        {
+            path: '/timetable',
+            name: 'Schedule',
+            component: Schedule
+        },
+        {
+            path: '/notice',
+            name: 'Notice',
+            component: Notice
+        },
+        {
+            path: '/notice/write',
+            name: 'NoticeWrite',
+            component: NoticeWrite
+        },
+        {
+            path: '/project',
+            name: 'Project',
+            component: Project
+        },
+        {
+            path: '/userlist',
+            name: 'UserList',
+            component: UserList
+        },
+        {
+            path: '/adminlist',
+            name: 'AdminList',
+            component: AdminList
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: test
+        }
+    ]
 })
