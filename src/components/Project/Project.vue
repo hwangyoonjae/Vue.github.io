72823e5bf4c1d4bfe53a730426aa2abf73094dd5
hwangyoonjae <hyj90632@gmail.com>
2022-02-06 22:01:36 +0900

    Project  data에 있는 값을 보여주도록 수정

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Project_component">
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column label="번호" prop="id"></el-table-column>
+    <el-table :data="items" style="width: 100%">
+      <el-table-column label="번호" prop="number"></el-table-column>
       <el-table-column label="프로젝트명" prop="title"></el-table-column>
       <el-table-column label="시작일" prop="start"></el-table-column>
       <el-table-column label="완료일" prop="finish"></el-table-column>
@@ -9,52 +9,28 @@
       <el-table-column label="상태" prop="state"></el-table-column>
       <el-table-column label="완료율" prop="success"></el-table-column>
     </el-table>
+    <div class="Project_write">
+        <el-button type="primary" @click="writeContent">프로젝트 등록하기</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import data from '@/data'
 
 export default {
   data() {
+    let items = data.ProjectContent.sort((a,b) => {return a.number - b.number})
+
     return {
-      tableData: [
-      {
-        id : 1,
-        title : 'Blue X-ray Enterprise',
-        start : '2016-05-03',
-        finish : '2016-05-04',
-        problem : '2',
-        state : 'soso',
-        success : '88%'
-      },
-      {
-        id : 2,
-        title : 'Blue X-ray DLP',
-        start : '2016-05-03',
-        finish : '2016-05-04',
-        problem : '0',
-        state : 'good',
-        success : '100%'
-      },
-      {
-        id : 3,
-        title : '전군PC보안체계',
-        start : '2016-05-03',
-        finish : '2016-05-04',
-        problem : '10',
-        state : 'just',
-        success : '50%'
-      },
-      {
-        id : 4,
-        title : 'DBSafer',
-        start : '2016-05-03',
-        finish : '2016-05-04',
-        problem : '20',
-        state : 'just',
-        success : '90%'
-      },
-      ]
+      items: items
+    }
+  },
+  methods: {
+    writeContent() {
+      this.$router.push({
+        path: '/project/write'
+      })
     }
   }
 }
@@ -63,5 +39,10 @@ export default {
 <style scoped>
 .Project_component {
   width: 100%;
+}
+
+.Project_write {
+  text-align: right;
+  margin: 10px;
 }
 </style>
