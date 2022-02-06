f9cc16a2180553513f588218249c4ed8294a9099
hwangyoonjae <hyj90632@gmail.com>
2022-02-06 23:58:59 +0900

    project 등록 페이지 입력 값 저장되어 테이블에 보이도록 구현

--- src/components/Project/Write.vue
+++ src/components/Project/Write.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="Noticewrite_component">
-    <div class="input-group" style="margin-bottom:10px;">
-      <el-input placeholder="프로젝트명을 입력하세요" v-model="title"></el-input>
-      <el-select v-model="state" placeholder="상태">
-        <el-option label="접수대기" value="접수대기"></el-option>
-        <el-option label="진행중" value="진행중"></el-option>
-        <el-option label="완료" value="완료"></el-option>
-      </el-select>
-    </div>
+  <div class="Projectwrite_component">
+    <el-form :model="projectForm" :rules="rules" ref="projectForm" label-width="120px">
+      <el-form-item label="프로젝트명" prop="title">
+        <el-input v-model="projectForm.title"></el-input>
+      </el-form-item>
+      <el-form-item label="프로젝트 기간" required>
+        <el-col :span="11" prop="start">
+          <el-date-picker type="date" placeholder="시작일" v-model="projectForm.start" style="width: 100%;"></el-date-picker>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11" prop="finish">
+          <el-date-picker type="date" placeholder="종료일" v-model="projectForm.finish" style="width: 100%;"></el-date-picker>
+        </el-col>   
+      </el-form-item>
+      <el-form-item label="상태구분" prop="state">
+        <el-select v-model="projectForm.state" placeholder="구분">
+          <el-option label="프리세일즈" value="프리세일즈"></el-option>
+          <el-option label="사업진행" value="사업진행"></el-option>
+          <el-option label="사업종료" value="사업종료"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
     <div class="button-group">
-      <el-button type="success" @click="updateMode ? updateContent() : uploadContent()">등록하기</el-button>
+      <el-button type="success" @click="updateMode ? updateContent() : uploadContent('formName')">등록하기</el-button>
       <el-button type="danger" @click="cancle">취소하기</el-button>
     </div>
   </div>
@@ -21,12 +34,28 @@ import data from '@/data'
 export default {
   data() {
     return {
-      title : '',
-      start : '2016-05-03',
-      finish : '2016-05-04',
-      problem : '',
-      state : '',
-      success : ''
+      projectForm: {
+        title : '',
+        start : '',
+        finish : '',
+        problem : '',
+        state : '',
+        success : ''
+      },
+      rules: {
+        title: [
+          { required: true, message: '프로젝트명을 입력해주세요.', trigger: 'blur' },
+        ],
+        state: [
+          { required: true, message: '프로젝트 상태구분을 선택해주세요.', trigger: 'change' },
+        ],
+        start: [
+          { required: true, message: '프로젝트 시작일을 선택해주세요.', trigger: 'change' },
+        ],
+        finish: [
+          { required: true, message: '프로젝트 종료일을 선택해주세요.', trigger: 'change' },
+        ],
+      }
     }
   },
   created() {
@@ -41,19 +70,22 @@ export default {
     uploadContent() {
       let items = data.ProjectContent.sort((a,b) => {return a.number - b.number})
       const number = items[3].number + 1
+
+      const date = dateFormat()
+
       data.ProjectContent.push({
         number: number,
-        title: this.title,
-        start: this.start,
-        finish: this.finish,
+        title: this.projectForm.title,
+        start: this.projectForm.start,
+        finish: this.projectForm.finish,
         problem: this.problem,
-        state: this.state,
+        state: this.projectForm.state,
         success: this.success
       })
       this.$router.push({
         path: '/project'
       })
-    },
+    }, 
     cancle() {
       this.$router.push({
         path: '/project'
