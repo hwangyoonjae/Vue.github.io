97ec987ffba0d39399f19831f94b3b3e1b570814
hwangyoonjae <hyj90632@gmail.com>
2022-02-15 23:44:15 +0900

    메인 페이지에서 프로젝트 리스트 등록버튼은 안보이고 볼 수만 있게 적용

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -47,7 +47,7 @@
 
 <script>
 import PanelGroup from './Panelgroup.vue'
-import Project from '../Project/Project.vue'
+import Project from './ProjectView.vue'
 import ProjectChart from '../Project/Chart.vue'
 import Timetable from '../Timetable/Timetable.vue'
 import Board from '../Board/Board.vue'
@@ -87,6 +87,10 @@ export default {
   border-bottom: 1px solid #000408;
   padding-left: 10px; 
   padding-right: 10px;
+}
+
+.project .Project_write {
+  display: none;
 }
 
 .page-group{
---
+++ src/components/Main/ProjectView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Project_component">
+    <el-table :data="items" style="width: 100%">
+      <el-table-column label="번호" prop="number"></el-table-column>
+      <el-table-column label="프로젝트명" prop="title"></el-table-column>
+      <el-table-column label="시작일" prop="start"></el-table-column>
+      <el-table-column label="완료일" prop="finish"></el-table-column>
+      <el-table-column label="이슈" prop="problem"></el-table-column>
+      <el-table-column label="구분" prop="state"></el-table-column>
+      <el-table-column label="완료율" prop="success"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import data from '@/data'
+
+export default {
+  data() {
+    let items = data.ProjectContent.sort((a,b) => {return a.number - b.number})
+
+    return {
+      items: items
+    }
+  },
+}
+</script>
+
+<style scoped>
+.Project_component {
+  width: 100%;
+}
+</style>
