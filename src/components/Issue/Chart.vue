3b237684d0d5c236a100a62dab00b78e37d18d13
hwangyoonjae <hyj90632@gmail.com>
2022-02-18 23:48:34 +0900

    Issue 차프 파이파츠로 구현

---
+++ src/components/Issue/Chart.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-chart class="chart" :option="option" />
+</template>
+
+<script>
+export default {
+  name: "Chart",
+  data() {
+    return {
+      option : {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [{
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: 'center'
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' }
+          ]
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  height: 320px;
+}
+</style>
