997c9c00e4fc717761117e9fa10afecb77b1c1a4
hwangyoonjae <hyj90632@gmail.com>
2022-02-21 19:09:07 +0900

    Project 행별 클릭 시 수정 혹은 삭제 진행

---
+++ src/components/Project/ProjectDetail.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-row :gutter="12">
+      <el-col :span="8">
+        <el-card shadow="never">
+          <el-form label-width="120px">
+            <el-form-item label="프로젝트명">
+              <el-input v-model="title"></el-input>
+            </el-form-item>
+            <el-form-item label="시작일">
+              <el-input v-model="start"></el-input>
+            </el-form-item>
+            <el-form-item label="완료일">
+              <el-input v-model="finish"></el-input>
+            </el-form-item>
+            <el-form-item label="상태구분" prop="state">
+              <el-select v-model="projectForm.state" placeholder="구분">
+                <el-option label="프리세일즈" value="프리세일즈"></el-option>
+                <el-option label="사업진행" value="사업진행"></el-option>
+                <el-option label="사업종료" value="사업종료"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <el-button type="primary" icon="el-icon-edit" @click="updateData">수정하기</el-button>
+          <el-button type="danger" icon="el-icon-document-delete" @click="deleteData">삭제하기</el-button>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import data from "@/data";
+
+export default {
+  name: "projectDetail",
+  data() {
+    const number = Number(this.$route.params.number);
+    const contentData = data.ProjectContent.filter(item => item.number === number)[0];
+    return {
+      number: number,
+      title: contentData.title,
+      start: contentData.start,
+      finish: contentData.finish,
+      state: contentData.state
+    }
+  },
+  methods: {
+    updateData() {
+      this.$router.push({
+        path: `/project/write/${this.number}`
+      })
+    },
+    deleteData() {
+      const content_index = data.ProjectContent.findIndex(item => item.number === this.number);
+      data.ProjectContent.splice(content_index, 1)
+      this.$router.push({
+        path: '/project'
+      })
+    },
+  }
+};
+</script>
+
+<style scoped>
+.text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    width: 60%;
+  }
+</style>
+
