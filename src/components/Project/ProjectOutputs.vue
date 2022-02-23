a722352916766f7ca6e80bbaa6e9223aa2d50b08
hwangyoonjae <hyj90632@gmail.com>
2022-02-23 23:30:04 +0900

    프로젝트별 데이터 보여주기 위한 페이지 초안 구성

---
+++ src/components/Project/ProjectOutputs.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="ProjectOutputs_Component">
+    <div class="row">
+      <div class="col-lg-12 mb-8">
+        <div class="card shadow mb-4">
+          <div class="card-body">
+            <div class="Project-area">
+              <div class="project_title">
+                <p>Blue X-ray Enterprise</p>
+              </div>
+              <BlueEnter />
+              <div style="text-align:right; margin-top:10px">
+                <el-link href="/project" :underline="false">더보기</el-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 mb-8">
+        <div class="card shadow mb-4">
+          <div class="card-body">
+            <div class="Project-area">
+              <div class="project_title">
+                <p>Blue X-ray DLP</p>
+              </div>
+              <div style="text-align:right; margin-top:10px">
+                <el-link href="/project" :underline="false">더보기</el-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 mb-8">
+        <div class="card shadow mb-4">
+          <div class="card-body">
+            <div class="Project-area">
+              <div class="project_title">
+                <p>전군PC보안체계</p>
+              </div>
+              <div style="text-align:right; margin-top:10px">
+                <el-link href="/project" :underline="false">더보기</el-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlueEnter from './ProjectOutputsinfo/BlueEnter.vue'
+import BlueDLP from './ProjectOutputsinfo/BlueDLP.vue'
+
+export default {
+  components: {
+    BlueEnter,
+    BlueDLP
+  }
+}
+</script>
+
+<style scoped>
+.ProjectOutputs_Component {
+  width: 100%;
+  height: 100%;
+  padding: 32px;
+  background-color: #f0f2f5;
+  position: relative;
+}
+</style>
