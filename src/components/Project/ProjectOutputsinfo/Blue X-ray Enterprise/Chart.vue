412c679fd49d6237bb01d53e7e21128b4acf7736
hwangyoonjae <yjhwang90632@gmail.com>
2022-02-24 17:52:02 +0900

    프로젝트별 필요한 차트 보여주기 초안

---
+++ src/components/Project/ProjectOutputsinfo/Blue X-ray Enterprise/Chart.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-chart class="chart" :option="option" />
+</template>
+
+<script>
+export default {
+  name: "Chart",
+  data() {
+    return {
+      option : {
+        legend: {},
+        tooltip: {},
+        dataset: {
+          source: [
+            ['product', '장애', '패치', '점검'],
+            ['2월', 43.3, 85.8, 93.7],
+            ['3월', 83.1, 73.4, 55.1],
+            ['4월', 86.4, 65.2, 82.5],
+            ['5월', 72.4, 53.9, 39.1]
+          ]
+        },
+        xAxis: { type: 'category' },
+        yAxis: {},
+        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  height: 320px;
+}
+</style>
