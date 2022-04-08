75f35f2d8a90d9192c05e944c816ca6c2bbeb55a
hwangyoonjae <hyj90632@gmail.com>
2022-04-08 10:30:43 +0900

    v-calendar 적용되는지 UI 확인

--- src/components/timetable/timetable.vue
+++ src/components/timetable/timetable.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="Schedule_Content">
     <el-calendar v-model="value"></el-calendar>
+    <v-calendar is-expanded />
   </div>
 </template>
 
 <script>
-
 export default {
     data() {
       return {
