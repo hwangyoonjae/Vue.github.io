54aaad137edb7d4202d66fab8517da3de825bf03
hwangyoonjae <hyj90632@gmail.com>
2022-04-08 15:22:14 +0900

    v-calendar 어떤방식으로 기능이 동작하는지 확인

--- src/components/timetable/timetable.vue
+++ src/components/timetable/timetable.vue
@@ -1,20 +1,66 @@
 <template>
   <div class="Schedule_Content">
-    <el-calendar v-model="value"></el-calendar>
-    <v-calendar is-expanded />
+    <v-date-picker :attributes="attrs"  is-expanded :value="null" color="red" is-range/>
+    <div class="Schedule_Button">
+      <el-button type="success">등록하기</el-button>
+    </div>
+    <v-date-picker :attributes="attributes" is-expanded :value="null" color="red" is-range/>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        value: new Date()
-      }
-    }
-  }
+  data() {
+    const todos = [
+      {
+        description: 'Take Noah to basketball practice.',
+        isComplete: false,
+        dates: { weekdays: 6 }, // Every Friday
+        color: 'red',
+      },
+    ];
+    return {
+      attrs: [
+        {
+          key: 'today',
+          //dot: true, 오늘 날짜 점 찍어줌.
+          //highlight: 'red', 오늘 날짜 동그라미 색 채워서 찍어줌.
+          highlight: {
+            color: 'purple',
+            fillMode: 'light', // solid, light, outline
+          },
+          dates: new Date(),
+        },
+      ],
+      incId: todos.length,
+      todos,
+    };
+  },
+  computed: {
+    attributes() {
+      return [
+        // Attributes for todos
+        ...this.todos.map(todo => ({
+          dates: todo.dates,
+          dot: {
+            color: todo.color,
+            class: todo.isComplete ? 'opacity-75' : '',
+          },
+          popover: {
+            label: todo.description,
+            visibility: 'hover',
+            hideIndicator: true
+          },
+          customData: todo,
+        })),
+      ];
+    },
+  },
+}
 </script>
 
 <style scoped>
-
+.Schedule_Button {
+  margin: 10px;
+}
 </style>
