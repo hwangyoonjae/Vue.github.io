e86ace17c0469ff69dd1dfd6d14e2ed4c9b26d31
hwangyoonjae <hyj90632@gmail.com>
2022-04-22 09:42:56 +0900

    이슈요청 페이지 검색 컴포넌트 추가

--- src/components/Task/IssueRequest/IssueRequestList.vue
+++ src/components/Task/IssueRequest/IssueRequestList.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="IssuerequestList_component">
-    <el-table :data="items" @row-click="rowClick">
-      <el-table-column label="번호" prop="number"></el-table-column>
-      <el-table-column label="제목" prop="title"></el-table-column>
-      <el-table-column label="내용" prop="component"></el-table-column>
-      <el-table-column label="작성자" prop="name"></el-table-column>
-      <el-table-column label="등록일" prop="created_at"></el-table-column>
-      <el-table-column label="구분" prop="division"></el-table-column>
-    </el-table>
-    <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+    <el-card class="box-card">
+      <div class="IssuerequestList_component_search">
+        <el-select v-model="value" placeholder="Select">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+        <el-input placeholder="검색하세요" v-model="input"></el-input>
+        <el-button type="primary" icon="el-icon-search">Search</el-button>
+      </div>
+    </el-card>
+    <el-card class="box-card">
+      <el-table :data="items" @row-click="rowClick">
+        <el-table-column label="번호" prop="number"></el-table-column>
+        <el-table-column label="제목" prop="title"></el-table-column>
+        <el-table-column label="내용" prop="component"></el-table-column>
+        <el-table-column label="작성자" prop="name"></el-table-column>
+        <el-table-column label="등록일" prop="created_at"></el-table-column>
+        <el-table-column label="구분" prop="division"></el-table-column>
+      </el-table>
+      <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-card>
     <div class="IssuerequestList_write">
         <el-button type="primary" @click="writeContent">등록하기</el-button>
     </div>
@@ -23,7 +34,15 @@ export default {
   data() {
     let items = data.IssueRequestContent.sort((a,b) => {return b.number - a.number})
     return {
-        items: items
+        items: items,
+        options: [{
+          value: 'Option1',
+          label: 'Option1'
+        }, {
+          value: 'Option2',
+          label: 'Option2'
+        }],
+        value: ''
       }
     },
     methods: {
@@ -50,7 +69,16 @@ export default {
 <style>
 .IssuerequestList_component {
   text-align: center;
-} 
+  margin: 10px;
+}
+
+.box-card {
+  margin: 10px 0px;
+}
+
+.IssuerequestList_component_search {
+  display: flex;
+}
 
 .IssuerequestList_write {
   text-align: right;
