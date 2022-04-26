f10366d8825341b716121d799ee3ccc3221cea0a
hwangyoonjae <hyj90632@gmail.com>
2022-04-26 23:01:54 +0900

    관리자, 사용자 목록 페이지 component별 구분

--- src/components/Person/UserList.vue
+++ src/components/Person/UserList.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="UserList_component">
+    <el-card class="box-card">
+      <div class="UserList_component_search">
+        <el-select v-model="value" placeholder="Select">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+        <el-input placeholder="검색하세요" v-model="input"></el-input>
+        <el-button type="primary" icon="el-icon-search">검색</el-button>
+      </div>
+    </el-card>
     <!--<el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))" style="width: 100%">-->
-    <el-table :data="items">
-      <el-table-column label="번호" prop="user_id"></el-table-column>
-      <el-table-column label="아이디" prop="id"></el-table-column>
-      <el-table-column label="이름" prop="name"></el-table-column>
-      <el-table-column label="이메일" prop="email"></el-table-column>
-      <el-table-column label="등록일" prop="created_at"></el-table-column>
-    </el-table>
-    <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+    <el-card class="box-card">
+      <el-table :data="items">
+        <el-table-column label="번호" prop="user_id"></el-table-column>
+        <el-table-column label="아이디" prop="id"></el-table-column>
+        <el-table-column label="이름" prop="name"></el-table-column>
+        <el-table-column label="이메일" prop="email"></el-table-column>
+        <el-table-column label="등록일" prop="created_at"></el-table-column>
+      </el-table>
+      <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-card>
   </div>
 </template>
 
@@ -44,5 +55,15 @@ export default {
 <style>
 .UserList_component {
   text-align: center;
+  margin: 10px;
 }
+
+.box-card {
+  margin: 10px 0px;
+}
+
+.UserList_component_search {
+  display: flex;
+}
+
 </style>
