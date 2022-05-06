6e6516c2d2d67ac42ca1f6252dbeb9e475a22534
hwangyoonjae <hyj90632@gmail.com>
2022-05-06 21:51:01 +0900

    요청사항 페이지 등록 시 입력안한 데이터 체크하도록 수정 적용

--- src/components/Task/IssueRequest/Write.vue
+++ src/components/Task/IssueRequest/Write.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="Issue_component">
-    <el-form status-icon label-width="120px" class="demo-ruleForm">
+    <el-form :model="Request_form" label-width="120px" class="demo-ruleForm" ref="Request_form" :rules="rules">
       <el-form-item label="구분" prop="division">
-        <el-select v-model="division" placeholder="선택">
+        <el-select v-model="Request_form.division" placeholder="선택">
           <el-option label="Blue X-ray Enterprise" value="Blue X-ray Enterprise"></el-option>
           <el-option label="Blue X-ray DLP" value="Blue X-ray DLP"></el-option>
           <el-option label="전군DLP" value="전군DLP"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="제목" prop="title">
-        <el-input type="text" v-model="title" autocomplete="off"></el-input>
+        <el-input type="text" v-model="Request_form.title" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="내용" prop="component">
-        <el-input type="text" v-model="component" autocomplete="off"></el-input>
+        <el-input type="text" v-model="Request_form.component" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="담당자" prop="name">
-        <el-input v-model="name"></el-input>
+        <el-input v-model="Request_form.name"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="success" @click="updateMode ? updateContent() : uploadContent()">등록하기</el-button>
-        <el-button @click="resetForm('ruleForm')">초기화</el-button>
+        <el-button type="success" @click="updateMode ? updateContent() : uploadContent('Request_form')">등록하기</el-button>
+        <el-button @click="resetForm('Request_form')">초기화</el-button>
         <el-button type="danger" @click="cancle">취소하기</el-button>
       </el-form-item>
     </el-form>
@@ -32,14 +32,29 @@ import data from '@/data'
 export default {
   data() {
     return {
-      title : '',
-      component : '',
-      name : '',
-      created_at: '2019-03-29',
-      division: '',
-      updated_at: null,
-      updateObject: null,
-      updateMode: this.$route.params.number > 0 ? true : false,
+      Request_form : {
+        title : '',
+        component : '',
+        name : '',
+        division: '',
+        updated_at: null,
+        updateObject: null,
+        updateMode: this.$route.params.number > 0 ? true : false,
+      },
+      rules: {
+        title : [
+          { required: true, message: '제목을 선택하세요.', trigger: 'change' }
+        ],
+        component : [
+          { required: true, message: '내용을 입력하세요.', trigger: 'blur' }
+        ],
+        name : [
+          { required: true, message: '이름을 입력하세요.', trigger: 'blur' }
+        ],
+        division : [
+          { required: true, message: '구분을 입력하세요.', trigger: 'blur' }
+        ]
+      }
     }
   },
   created() {
