b1ebb6974752f08778f0671e6f787075c3e3e1cc
hwangyoonjae <hyj90632@gmail.com>
2022-06-27 19:04:34 +0900

    로그인 페이지 div로 영역 하나 더 만들어서 가운데 정렬하기 위해 초안 작성

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,34 +1,32 @@
 <template>
   <div class="Login_Component">
-    <el-row>
-      <el-col :span="4">
-        <div class="grid-content"></div>
-      </el-col>
-      <el-col :span="16">
-        <div class="grid-content">
-          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-            <el-form-item label="ID" prop="id">
-              <el-input type="username" v-model="ruleForm.id" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="Password" prop="pass">
-              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">로그인 하기</el-button>
-            </el-form-item>
-            <el-form-item>
-              <p>
-                <span> 회원이 아니신가요?</span>
-                <b-link to="/main/signup">지금 가입하세요</b-link>
-              </p>
-            </el-form-item>
-          </el-form>
-        </div>
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content"></div>
-      </el-col>
-    </el-row>
+    <div class="Login_Component_part">
+      <el-row>
+        <el-col :span="20">
+          <div class="grid-content">
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+              <el-form-item label="ID" prop="id">
+                <el-input type="username" v-model="ruleForm.id" autocomplete="off" placeholder="Enter Username..."></el-input>
+              </el-form-item>
+              <el-form-item label="Password" prop="pass">
+                <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="Enter Password..."></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button class="Login_Component_Button" type="primary" @click="submitForm('ruleForm')">Login</el-button>
+              </el-form-item>
+              <el-form-item>
+                <p>
+                  <span> 회원이 아니신가요?</span>
+                  <b-link to="/main/signup">지금 가입하세요</b-link>
+                </p>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+
+      </el-row>
+    </div>
+    
   </div>
 </template>
 
@@ -86,20 +84,33 @@
 </script>
 
 <style>
-.el-row {
-  margin-bottom: 20px;
-}
-
 .el-col {
   border-radius: 4px;
 }
 
 .grid-content {
+  width: 100%;
   border-radius: 4px;
   min-height: 36px;
+  padding: 30px;
 }
 
 .row-bg {
   padding: 10px 0;
+}
+
+.Login_Component {
+  padding: 50px;
+}
+
+.Login_Component_part {
+  background-color: #F2F2F2;
+  width: 100%;
+}
+
+.Login_Component_Button {
+  width: 100%;
+  padding: 10px;
+  border-radius: 10px;
 }
 </style>
