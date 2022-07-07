8629f778923a6ed8fabdfd81f1499e99019897ef
hwangyoonjae <hyj90632@gmail.com>
2022-07-07 13:18:00 +0900

    프로젝트 등록 시 빈칸이 있으면 input 박스 밑에 입력하라는 메세지가 나오도록 수정

--- src/components/Project/Write.vue
+++ src/components/Project/Write.vue
@@ -4,28 +4,28 @@
       <el-col :span="16">
         <el-card class="box-card">
           <div class="grid_content_input">
-            <el-form :rules="rules" label-width="120px">
+            <el-form :model="Project_form" ref="Project_form" :rules="rules" label-width="120px">
               <el-form-item label="프로젝트명" prop="title">
-                <el-input v-model="title"></el-input>
+                <el-input v-model="Project_form.title"></el-input>
               </el-form-item>
               <el-form-item label="프로젝트 기간" required>
                 <el-col :span="11" prop="start">
-                  <el-date-picker type="date" placeholder="시작일" v-model="start" format="yyyy/MM/dd" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                  <el-date-picker type="date" placeholder="시작일" v-model="Project_form.start" format="yyyy/MM/dd" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
                 </el-col>
                 <el-col class="line" :span="2">-</el-col>
                 <el-col :span="11" prop="finish">
-                  <el-date-picker type="date" placeholder="종료일" v-model="finish" format="yyyy/MM/dd" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                  <el-date-picker type="date" placeholder="종료일" v-model="Project_form.finish" format="yyyy/MM/dd" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
                 </el-col>   
               </el-form-item>
               <el-form-item label="상태구분" prop="state">
-                <el-select v-model="state" placeholder="구분">
+                <el-select v-model="Project_form.state" placeholder="구분">
                   <el-option label="프리세일즈" value="프리세일즈"></el-option>
                   <el-option label="사업진행" value="사업진행"></el-option>
                   <el-option label="사업종료" value="사업종료"></el-option>
                 </el-select>
               </el-form-item>
               <el-form-item label="라이센스" prop="license">
-                <el-input v-model="license"></el-input>
+                <el-input v-model="Project_form.license"></el-input>
               </el-form-item>
             </el-form>
           </div>
@@ -33,7 +33,7 @@
       </el-col>
     </el-row>
     <div class="button-group">
-      <el-button type="success" @click="updateMode ? updateContent() : uploadContent()">등록하기</el-button>
+      <el-button type="success" @click="updateMode ? updateContent() : uploadContent('Project_form')">등록하기</el-button>
       <el-button type="danger" @click="cancle">취소하기</el-button>
     </div>
   </div>
@@ -46,22 +46,33 @@ export default {
   name: 'Write',
   data() {
     return {
-      title : '',
-      start : '',
-      finish : '',
-      problem : '7',        
-      state : '',
-      success : '100%',
-      license : '',
-      updatedAt: null,
-      updateObject: null,
-      updateMode: this.$route.params.number > 0 ? true : false,
+      Project_form : {
+        title : '',
+        start : '',
+        finish : '',
+        problem : '7',        
+        state : '',
+        success : '100%',
+        license : '',
+        updatedAt: null,
+        updateObject: null,
+        updateMode: this.$route.params.number > 0 ? true : false,
+      },
       rules: {
+        title : [
+          { required: true, message: '제목을 선택하세요.', trigger: 'blur' }
+        ],
         start: [
           { required: true, message: '프로젝트 시작일을 선택해주세요.', trigger: 'change' },
         ],
         finish: [
           { required: true, message: '프로젝트 종료일을 선택해주세요.', trigger: 'change' },
+        ],
+        state : [
+          { required: true, message: '상태를 입력하세요.', trigger: 'change' }
+        ],
+        license : [
+          { required: true, message: '라이센스을 입력하세요.', trigger: 'blur' }
         ],
       }
     }
@@ -78,23 +89,35 @@ export default {
     }
   },
   methods:{
-    uploadContent() {
-      let items = data.ProjectContent.sort((a,b) => {return b.number - a.number})
-      const number = items[0].number + 1
-
-      data.ProjectContent.push({
-        number: number,
-        title: this.title,
-        start: this.start,
-        finish: this.finish,
-        problem: this.problem,
-        state: this.state,
-        success: this.success,
-        license: this.license
-      })
-      this.$router.push({
-        path: '/project'
-      })
+    uploadContent(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('등록되었습니다.');
+          const baseURI = 'http://localhost:8443';
+          var data = {
+            division : this.Project_form.division,
+            title : this.Project_form.title,
+            component : this.Project_form.component,
+            name : this.Project_form.name,
+            state : this.Project_form.state,
+            level : this.Project_form.level
+          }
+          this.$axios.post(`${baseURI}/api/project`, data)
+          .then(result => {
+            console.log(result)
+            this.$router.push({
+              path: '/project'
+            })
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        } else {
+            console.log('error submit!!');
+            return false;
+          }
+        }
+      );
     },
     updateContent() {
       this.updateObject.title = this.title;
