03fe5d034759ba2225fda41fd16c1ae225fc4c87
hwangyoonjae <hyj90632@gmail.com>
2022-07-10 22:35:11 +0900

    고객리스트 등록화면에 제목 추가

--- src/components/Task/Customer/Write.vue
+++ src/components/Task/Customer/Write.vue
@@ -3,6 +3,9 @@
     <el-row>
       <el-col :span="16">
         <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>고객 등록</span>
+          </div>
           <div class="grid_content_input">
             <el-form :model="Customer_form" m status-icon label-width="120px" class="demo-ruleForm" ref="Customer_form" :rules="rules">
               <el-form-item label="구분" prop="division">
