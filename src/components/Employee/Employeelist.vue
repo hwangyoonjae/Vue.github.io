375c63cb2404bf3842e532de22149d0865170f5a
hwangyoonjae <hyj90632@gmail.com>
2023-10-10 20:57:58 +0900

    사원리스트 하드코딩한 데이터로 출력되는지 확인하기 위해 적용

--- src/components/Employee/Employeelist.vue
+++ src/components/Employee/Employeelist.vue
@@ -10,12 +10,12 @@
       </div>
     </el-card>
     <el-card class="box-card">
-      <el-table :data="displayData" @row-click="rowClick">
-        <el-table-column label="번호" prop="number"></el-table-column>
-        <el-table-column label="이름" prop="title"></el-table-column>
+      <el-table :data="items" @row-click="rowClick">
+        <el-table-column label="번호" prop="user_id"></el-table-column>
+        <el-table-column label="이름" prop="name"></el-table-column>
         <el-table-column label="부서" prop="component"></el-table-column>
         <el-table-column label="직급" prop="name"></el-table-column>
-        <el-table-column label="등록일" prop="date"></el-table-column>
+        <el-table-column label="등록일" prop="created_at"></el-table-column>
       </el-table>
       <el-pagination :page-size="pageSize" layout="prev, pager, next" @current-change="handleCurrentChange" :total="items.length"></el-pagination>
       <div class="Employee_write">
@@ -33,20 +33,22 @@ import data from '@/data'
 export default {
   name : 'Employee',
   data() {
-    let items = data.NoticeContent.sort((a,b) => {return b.number - a.number})
+    let items = data.User.sort((a,b) => {return b.user_id - a.user_id})
+    
+    const user = Number(this.$route.params.user_id);
+    const contentData = data.User.filter(item => item.user_id === user)[0];
     return {
         items: items,
-        page: 1,
-        pageSize: 10,
         options: [{
-          value: '제목',
-          label: '제목'
+          value: '아이디',
+          label: '아이디'
         }, {
-          value: '내용',
-          label: '내용'
+          value: '이름',
+          label: '이름'
+        }, {
+          value: '이메일',
+          label: '이메일'
         }],
-        value: '',
-        input: ''
       }
     },
     computed: {
