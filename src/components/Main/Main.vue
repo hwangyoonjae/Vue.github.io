85602aa007c5ed5f69fe2abd9d5d0c4cfdb96821
hwangyoonjae <hyj90632@gmail.com>
2023-10-23 01:49:57 +0900

    근무현황, 주간테이블, 최근근태내역 초안으로 기본코드 작성

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -3,8 +3,8 @@
     <PanelGroup />
 
     <div class="row">
-      <div class="col-lg-6 mb-4">
-        <div class="card shadow mb-4">
+      <div class="col-lg-6 mb-3">
+        <div class="card shadow mb-3">
           <el-card class="box-card" style="padding: 10px; height: 300px;">
             <div slot="header" class="clearfix">
               <i class="el-icon-date"></i>
@@ -25,8 +25,8 @@
         </div>
       </div>
 
-      <div class="col-lg-6 mb-4">
-        <div class="card shadow mb-4">
+      <div class="col-lg-6 mb-3">
+        <div class="card shadow mb-3">
           <el-card class="box-card" style="padding: 10px; height: 300px;">
             <div slot="header" class="clearfix">
               <i class="el-icon-circle-check"></i>
@@ -65,8 +65,75 @@
           </el-card>
         </div>
       </div>
-
     </div>
+
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+          <div class="card shadow mb-3">
+            <el-card class="box-card" style="padding: 10px; height: 300px;">
+              <div slot="header" class="clearfix">
+                <i class="el-icon-monitor"></i>
+                근무현황
+                <el-button style="float: right; padding: 3px 0" type="text">더보기</el-button>
+              </div>
+              <div class="main">
+                <el-timeline :reverse="reverse">
+                  <el-timeline-item v-for="(activity, index) in activities" :key="index" :timestamp="activity.timestamp">
+                    {{activity.content}}
+                  </el-timeline-item>
+                </el-timeline>  
+              </div>
+            </el-card>
+          </div>
+        </div>
+
+        <div class="col-lg-8 mb-3">
+          <div class="card shadow mb-3">
+            <el-card class="box-card" style="padding: 10px; height: 300px;">
+              <div slot="header" class="clearfix">
+                <i class="el-icon-date"></i>
+                주간테이블
+                <el-button style="float: right; padding: 3px 0" type="text">더보기</el-button>
+              </div>
+              <div class="main">
+                <el-table :data="timetableData" style="width: 100%" border :row-class-name="rowClassName">
+                  <el-table-column prop="time" label="시간" width="120"></el-table-column>
+                  <el-table-column prop="mon" label="월"></el-table-column>
+                  <el-table-column prop="tue" label="화"></el-table-column>
+                  <el-table-column prop="wed" label="수"></el-table-column>
+                  <el-table-column prop="thu" label="목"></el-table-column>
+                  <el-table-column prop="fri" label="금"></el-table-column>
+                </el-table>
+              </div>
+            </el-card>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12 mb-3">
+          <div class="card shadow mb-3">
+            <el-card class="box-card" style="padding: 10px; height: 300px;">
+              <div slot="header" class="clearfix">
+                <i class="el-icon-date"></i>
+                최근근태내역
+                <el-button style="float: right; padding: 3px 0" type="text">더보기</el-button>
+              </div>
+              <div class="main">
+                <el-table :data="attendanceData" style="width: 100%" border :row-class-name="rowClassName">
+                  <el-table-column prop="type" label="신청항목"></el-table-column>
+                  <el-table-column prop="category" label="신청구분"></el-table-column>
+                  <el-table-column prop="startDate" label="시작일자"></el-table-column>
+                  <el-table-column prop="endDate" label="종료일자"></el-table-column>
+                  <el-table-column prop="createdDate" label="작성일"></el-table-column>
+                  <el-table-column prop="status" label="상태"></el-table-column>
+                </el-table>
+              </div>
+            </el-card>
+          </div>
+        </div>
+      </div>
+
   </div>
 </template>
 
@@ -90,6 +157,65 @@ export default {
       Detail: true,
       chart: false,
       serverTime: '',
+      reverse: true,
+        activities: [
+        {
+          content: 'Event start',
+          timestamp: '2018-04-15'
+        }, 
+        {
+          content: 'Approved',
+          timestamp: '2018-04-13'
+        }, 
+        {
+          content: 'Success',
+          timestamp: '2018-04-11'
+        }
+      ],
+      timetableData: [
+        {
+          time: '09:00 AM',
+          mon: 'Math',
+          tue: 'History',
+          wed: 'Science',
+          thu: 'English',
+          fri: 'Physical Ed.',
+        },
+        {
+          time: '10:00 AM',
+          mon: 'Math',
+          tue: 'History',
+          wed: 'Science',
+          thu: 'English',
+          fri: 'Physical Ed.',
+        },
+        {
+          time: '11:00 AM',
+          mon: 'Math',
+          tue: 'History',
+          wed: 'Science',
+          thu: 'English',
+          fri: 'Physical Ed.',
+        }
+      ],
+      attendanceData: [
+        {
+          type: '휴가',
+          category: '연차',
+          startDate: '2023-09-01',
+          endDate: '2023-09-05',
+          createdDate: '2023-08-15',
+          status: '미결재',
+        },
+        {
+          type: '외근',
+          category: '장소 이전',
+          startDate: '2023-09-10',
+          endDate: '2023-09-12',
+          createdDate: '2023-08-25',
+          status: '결재 완료',
+        }
+      ]
     }
   },
   methods: {
@@ -111,6 +237,24 @@ export default {
       const formattedTime = `${hours % 12}:${minutes}:${String(seconds).padStart(2, '0')}`;
       this.serverTime = formattedTime;
     },
+    showConfirmation(title, action) {
+      const confirmationMessage = action === 'checkIn' ? '출근 확인' : '퇴근 확인';
+      alert(confirmationMessage);
+    },
+    rowClassName({ row }) {
+      if (row.status === '미결재') {
+        return 'unprocessed-row';
+      } else if (row.status === '결재 완료') {
+        return 'processed-row';
+      }
+      return ''; // 기본 클래스
+    },
+    requestOvertime() {
+      // 연장근무 신청 로직을 여기에 추가
+    },
+    requestLeave() {
+      // 휴(무)일 근무 신청 로직을 여기에 추가
+    }
   },
   created() {
     this.updateServerTime(); // 컴포넌트 생성 시 현재 시간 업데이트
@@ -155,5 +299,13 @@ export default {
 
 .current-time > h3 {
   margin: 0px 10px;
+}
+
+.unprocessed-row {
+  background-color: lightgray;
+}
+
+.processed-row {
+  background-color: lightblue;
 }
 </style>
