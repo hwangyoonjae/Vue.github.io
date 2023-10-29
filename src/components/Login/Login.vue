9104aaf190d7c614d155c955dfabf4811bb11b27
hwangyoonjae <hyj90632@gmail.com>
2023-10-30 00:14:04 +0900

    로그인 템플릿 수정 추후에 스크립트의 있는 변수랑 맞추기 위해 추가 수정 필요

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="Login_Component">
-    <div class="Login_Component_part">
-      <el-row type="flex" justify="center" align="middle" style="height: 100vh;">
-        <el-col :span="20">
-          <div class="Login_Component_header">
-            <h1>로그인</h1>
-          </div>
-          <div class="grid-content">
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-              <el-form-item label="ID" prop="id">
-                <el-input type="username" v-model="ruleForm.id" autocomplete="off" placeholder="Enter Username..."></el-input>
-              </el-form-item>
-              <el-form-item label="PW" prop="pass">
-                <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="Enter Password..."></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button class="Login_Component_Button" type="primary" @click="submitForm('ruleForm')">로그인</el-button>
-              </el-form-item>
-              <el-form-item>
-                <p>
-                  <span> 회원이 아니신가요?</span>
-                  <b-link to="/signup">가입하기</b-link>
-                </p>
-              </el-form-item>
-            </el-form>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
+  <div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>회원가입</h1>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>회원가입</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>로그인</h1>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">비밀번호를 잊으셨나요?</a>
+			<button>로그인</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>어서와요!</h1>
+				<p>계정이 존재하면 지금 발로 로그인 하세요.</p>
+				<button class="ghost" id="signIn">로그인</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>환영합니다!</h1>
+				<p>계정을 만들고 우리 서비스의 일부가 되세요.</p>
+				<button class="ghost" id="signUp">회원가입</button>
+			</div>
+		</div>
+	</div>
   </div>
 </template>
 
@@ -98,34 +102,267 @@
         }
       });
     },
+  },
+  mounted() {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
   }
 }
 </script>
 
 <style scoped>
-.el-col {
-  border-radius: 4px;
+body {
+  background: #f6f5f7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Montserrat', sans-serif;
+  height: 100vh;
+  margin: -20px 0 50px;
 }
 
-.grid-content {
-  width: 100%;
-  border-radius: 4px;
-  min-height: 36px;
-  padding: 30px;
+h1 {
+  font-weight: bold;
+  margin: 0;
 }
 
-.Login_Component_part {
-  background-color: #F2F2F2;
-  width: 100%;
-}
-
-.Login_Component_header {
+h2 {
   text-align: center;
 }
 
-.Login_Component_Button {
+p {
+  font-size: 14px;
+  font-weight: 100;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  margin: 20px 0 30px;
+}
+
+span {
+  font-size: 12px;
+}
+
+a {
+  color: #333;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 15px 0;
+}
+
+button {
+  border-radius: 20px;
+  border: 1px solid #5882FA;
+  background-color: #5882FA;
+  color: #FFFFFF;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
+}
+
+button:active {
+  transform: scale(0.95);
+}
+
+button:focus {
+  outline: none;
+}
+
+button.ghost {
+  background-color: transparent;
+  border-color: #FFFFFF;
+}
+
+form {
+  background-color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
+}
+
+input {
+  background-color: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
   width: 100%;
-  padding: 10px;
-  border-radius: 4px;
+}
+
+.container {
+  background-color: #fff;
+  border-radius: 10px;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+      0 10px 10px rgba(0,0,0,0.22);
+  position: relative;
+  overflow: hidden;
+  width: 768px;
+  max-width: 100%;
+  min-height: 480px;
+}
+
+.form-container {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+}
+
+.sign-in-container {
+  left: 0;
+  width: 50%;
+  z-index: 2;
+}
+
+.container.right-panel-active .sign-in-container {
+  transform: translateX(100%);
+}
+
+.sign-up-container {
+  left: 0;
+  width: 50%;
+  opacity: 0;
+  z-index: 1;
+}
+
+.container.right-panel-active .sign-up-container {
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+  animation: show 0.6s;
+}
+
+@keyframes show {
+  0%, 49.99% {
+    opacity: 0;
+    z-index: 1;
+  }
+  
+  50%, 100% {
+    opacity: 1;
+    z-index: 5;
+  }
+}
+
+.overlay-container {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.6s ease-in-out;
+  z-index: 100;
+}
+
+.container.right-panel-active .overlay-container{
+  transform: translateX(-100%);
+}
+
+.overlay {
+  background: #5882FA;
+  background: -webkit-linear-gradient(to right, #81BEF7, #5882FA);
+  background: linear-gradient(to right, #81BEF7, #5882FA);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 0 0;
+  color: #FFFFFF;
+  position: relative;
+  left: -100%;
+  height: 100%;
+  width: 200%;
+    transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.container.right-panel-active .overlay {
+    transform: translateX(50%);
+}
+
+.overlay-panel {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 40px;
+  text-align: center;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.overlay-left {
+  transform: translateX(-20%);
+}
+
+.container.right-panel-active .overlay-left {
+  transform: translateX(0);
+}
+
+.overlay-right {
+  right: 0;
+  transform: translateX(0);
+}
+
+.container.right-panel-active .overlay-right {
+  transform: translateX(20%);
+}
+
+.social-container {
+  margin: 20px 0;
+}
+
+.social-container a {
+  border: 1px solid #DDDDDD;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  height: 40px;
+  width: 40px;
+}
+
+footer {
+    background-color: #222;
+    color: #fff;
+    font-size: 14px;
+    bottom: 0;
+    position: fixed;
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 999;
+}
+
+footer p {
+    margin: 10px 0;
+}
+
+footer i {
+    color: red;
+}
+
+footer a {
+    color: #3c97bf;
+    text-decoration: none;
 }
 </style>
