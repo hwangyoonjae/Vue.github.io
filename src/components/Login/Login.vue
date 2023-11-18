ede7bbdb3ece24b80a43d838773bbaa3c1b34fd0
hwangyoonjae <hyj90632@gmail.com>
2023-11-19 02:04:51 +0900

    submitSignupForm공백제거('ruleForm')

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -23,7 +23,7 @@
         </el-form-item>
         <el-form-item>
           <!--<el-button type="primary" @click="updateMode ? updateContent() : uploadContent()">회원가입</el-button>-->
-          <el-button type="primary" @click="submitSignupForm ('ruleForm')">회원가입</el-button>
+          <el-button type="primary" @click="submitSignupForm('ruleForm')">회원가입</el-button>
           <el-button @click="resetForm('ruleForm')">초기화</el-button>
         </el-form-item>
       </el-form>
